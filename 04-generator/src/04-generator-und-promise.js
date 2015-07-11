8ffe223d82415b2bec5569c5922c0b45ca9ff4c8
Arthur Trauter <arthur.trauter@mannheimer.de>
2015-07-11 10:24:37 +0200

    no functional changes only comments added

--- 04-generator/src/04-generator-und-promise.js
+++ 04-generator/src/04-generator-und-promise.js
@@ -8,18 +8,27 @@ var generatorFn = function*(num) {
   };
 };
 
+// A: erstelle eine Generator Funktion
+// B: aufruf der resume funktion mit Übergabe des ersten promises
+//    generiert durch die generator funktion
+// C: neue Funktion bekommt ein Promise übergeben
+// D: wenn das promise erfüllt ist wird
+// E: das Ergebnis ausgegeben und anschließextend
+// F: der Generator um ein neues promise gebeten
+// G: solange der generator weitere promises erzeugen kann
+// H: wird die resume funktion weiterhin rekursiv aufgerufen
 function async(generatorFn) {
-  var gen = generatorFn(maxLoop);
-  var resume = function(promise) {
-    return promise.then(function(text) {
-      console.log(text);
-      var next = gen.next(text);
-      if (!next.done) {
-        return resume(next.value);
+  var gen = generatorFn(maxLoop); // A
+  var resume = function(promise) { // C
+    return promise.then(function(text) { // D
+      console.log(text); // E
+      var next = gen.next(text); // F
+      if (!next.done) { // G
+        return resume(next.value); // H
       }
     });
   };
-  return resume(gen.next().value);
+  return resume(gen.next().value); // B
 };
 
 async(generatorFn);
