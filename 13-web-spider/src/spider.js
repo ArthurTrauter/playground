f0938f3d45f520d49ebc6a26bad5cdf74bee7779
ArthurTrauter <arthur.trauter@googlemail.com>
2015-07-13 22:14:46 +0200

    new spider app with callback hell implementation

---
+++ 13-web-spider/src/spider.js
@@ -0,0 +1,57 @@
+var fs = require('fs');
+var path = require('path');
+
+var mkdirp = require('mkdirp');
+var request = require('request');
+
+var utilities = require('./utilities');
+
+function getUrlFromArgs() {
+  if (process.argv.length > 2) {
+    return process.argv[2];
+  }
+  console.log('Url Übergabeparameter fehlt');
+  process.exit(1);
+};
+
+function spider(url, callback) {
+  var filename = __dirname + '/../files/';
+  filename += utilities.urlToFilename(url);
+  
+  fs.stat(filename, function (err, stats) {
+    if (err) {
+      console.log('Downloading ' + url);
+      request(url, function (err, response, body) {
+        if (err) {
+          callback(err);
+        } else {
+          mkdirp(path.dirname(filename), function (err) {
+            if (err) {
+              callback(err);
+            } else {
+              fs.writeFile(filename, body, function (err) {
+                if (err) {
+                  callback(err);
+                } else {
+                  callback(null, filename, true);
+                }
+              });
+            }
+          });
+        }
+      });
+    } else {
+      callback(null, filename, false);
+    }
+  });
+};
+
+spider(getUrlFromArgs(), function (err, filename, downloaded) {
+  if (err) {
+    return console.log(err);
+  }
+  if (downloaded) {
+    return console.log('Completed the download of "' + filename + '"');
+  }
+  return console.log('"' + filename + '" was allready downloaded');
+});
