a5b51980ebe139d378db7b0dbc884ce4ac527830
Arthur Trauter <arth@localhost.localdomain>
2015-08-02 22:32:39 +0200

    code mit closures kapseln anstatt mit strings

---
+++ 19-js-efective/lib/27-example-0.js
@@ -0,0 +1,41 @@
+(function() {
+  'use strict';
+
+  var async = require('async');
+
+  function repeat(n, action) {
+    for (var i = 0; i < n; i++) {
+      action();
+    }
+  }
+
+  function benchmark() {
+    var start = [],
+      end = [],
+      timings = [];
+
+    repeat(10, function() {
+      start.push(Date.now());
+      f();
+      end.push(Date.now());
+    });
+    for (var i = 0, n = start.length; i < n; i++) {
+      timings[i] = end[i] - start[i];
+    }
+    return timings;
+  }
+
+  function f() {
+    var r = randomIntInc(100, 1000);
+    setTimeout(function(r) {
+      console.log('Dieser Timeout dauert %s msec', r);
+    }, r, r);
+  }
+
+  function randomIntInc(low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
+  }
+
+  console.log(benchmark());
+
+}());
