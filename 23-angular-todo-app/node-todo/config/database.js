f7464cc98bec5a6e3010ca1d9bc06a5489a3659d
Arthur Trauter <arthur.trauter@googlemail.com>
2015-08-12 22:08:54 +0200

    example heruntergeladen und versucht zum laufen zu kriegen. hat nicht geklappt! na super!

---
+++ 23-angular-todo-app/node-todo/config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+	// the database url to connect
+	//url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
+	url : 'mongodb://ds055802.mongolab.com:55802/playground'
+}
