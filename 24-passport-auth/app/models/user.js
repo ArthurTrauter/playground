ccd0922114745a8011027a938cc88630316e9542
Arthur Trauter <arthur.trauter@mannheimer.de>
2015-08-14 09:51:37 +0200

    new db-model for user handling

---
+++ 24-passport-auth/app/models/user.js
@@ -0,0 +1,47 @@
+(function() {
+  'use strict';
+
+  var mongoose = require('mongoose');
+  var bcrypt = require('bcrypt');
+
+  // define shema for our user model
+  var userSchema = mongoose.Schema({
+
+    local: {
+      email: String,
+      password: String
+    },
+    facebook: {
+      id: String,
+      token: String,
+      email: String,
+      name: String
+    },
+    twitter: {
+      id: String,
+      token: String,
+      displayName: String,
+      username: String
+    },
+    google: {
+      id: String,
+      token: String,
+      email: String,
+      name: String
+    }
+
+  });
+
+  // generating a hash
+  userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(pasword, bcrypt.genSaltSync(8), null);
+  };
+
+  // generating a hash
+  userSchema.methods.validPasswod = function(password) {
+    return bcrypt.compareSync(pasword, this.local.password);
+  };
+
+  module.exports = mongoose.model('User', userSchema);
+
+}());
