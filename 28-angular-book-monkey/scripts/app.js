80054bd4b35fd9709fcdd9c838ef5351b98c9aba
Arthur Trauter <arthur.trauter@googlemail.com>
2015-08-25 22:23:37 +0200

    new book list impl

--- 28-angular-book-monkey/scripts/app.js
+++ 28-angular-book-monkey/scripts/app.js
@@ -1,8 +1,13 @@
   var bmApp = angular.module('bmApp', ['ngRoute']);
 
   bmApp.config(function($routeProvider) {
-    $routeProvider.when('/books/:isbn', {
-      templateUrl: './scripts/templates/book_details.html',
-      controller: 'BookDetailsCtrl'
-    });
+    $routeProvider
+      .when('/books/:isbn', {
+        templateUrl: './scripts/templates/book_details.html',
+        controller: 'BookDetailsCtrl'
+      })
+      .when('/books', {
+        templateUrl: './scripts/templates/book_list.html',
+        controller: 'BookListCtrl'
+      });
   });
