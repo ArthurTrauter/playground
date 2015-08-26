246ba29ff629b0f0067fd1989c235cfe452af189
Arthur Trauter <arthur.trauter@mannheimer.de>
2015-08-26 07:27:01 +0200

    tried to test with protractor. have to change karma-test syntax to protractor syntax

---
+++ 28-angular-book-monkey/README.md
@@ -0,0 +1,3 @@
+Protractor installation behind a proxy.
+You have to update the webdriver-manager with the folowing command:
+webdriver-manager --proxy http://10.120.11.11:8080 update --standalone
--- 28-angular-book-monkey/tests/e2e/templates/book_details.spec.js
+++ 28-angular-book-monkey/tests/e2e/templates/book_details.spec.js
@@ -1,11 +1,12 @@
 describe("E2E: book details view", function() {
 
   beforeEach(function() {
-    browser().navigateTo('/');
+    browser.get('http://localhost:8080/');
   });
 
   it('should show the correct book details', function() {
-    browser().navigateTo('#/books/978-3-89864-728-1');
+    // browser().navigateTo('#/books/978-3-89864-728-1');
+    browser.get('http://localhost:8080/#/books/978-3-89864-728-1');
 
     expect(element('.bm-book-title').html())
       .toBe('JavaScript für Enterprise-Entwickler');
