8a0954b44491757a9bd219d929e0c5cc01888114
Arthur Trauter <arthur.trauter@googlemail.com>
2016-02-09 08:46:56 +0100

    thema 62

--- 19-js-efective/lib/61-callbacks-0.js
+++ 19-js-efective/lib/61-callbacks-0.js
@@ -1,25 +1,24 @@
-
 var fs = require('fs');
 
 var path = __dirname + "/21-example-1.js";
 
-fs.stat(path, function (err, stats) {
-   if (err) {
-      throw new Error(err);
-   }
-   console.log(stats);
-   myReadFunction();
+fs.stat(path, function(err, stats) {
+  if (err) {
+    throw new Error(err);
+  }
+  console.log(stats);
+  myReadFunction();
 });
 
 function myReadFunction() {
-   fs.readFile(path, function (err, file) {
-      console.log(file.toString());
-   });
+  fs.readFile(path, function(err, file) {
+    console.log(file.toString());
+  });
 }
 
-fs.stat(path, function (err, stats) {
-   console.log(stats);
-   fs.readFile(path, function (err, file) {
-      console.log(file.toString());
-   })
-})
+fs.stat(path, function(err, stats) {
+  console.log(stats);
+  fs.readFile(path, function(err, file) {
+    console.log(file.toString());
+  });
+});
---
+++ 19-js-efective/lib/64-recursion-asynch-loop-0.js
@@ -0,0 +1,58 @@
+// Für asynchrone Schleifen sollen rekursive Aufrufe verwendet werden
+// Damit die Aufrufe nicht alle gleichzeitig passieren kann man die Schleifen Funktionalität
+// in eine eigene Funktion auslagern. Siehe Beispiel:
+
+var request = require('request');
+
+
+
+var urls = ["http://www.google.com", "http://www.github.com", "http://www.npmjs.org"];
+
+
+
+function downloadAsync(url, onsuccess, onfailure) {
+  var options = {
+    "url": url,
+    "method": "GET",
+    "proxy": "http://10.120.11.11:8080"
+  };
+
+  request(options, function(error, response) {
+    if (!error && response.statusCode === 200) {
+      onsuccess("statusCode " + response.statusCode);
+    } else {
+      onfailure(error);
+    }
+  });
+
+}
+
+
+
+function downloadOneAsync(urls, onsuccess, onfailure) {
+  var n = urls.length;
+
+  function tryNextURL(i) {
+    console.log(urls[i]);
+    if (i >= n) {
+      onfailure("all downloads failed");
+      return;
+    }
+    downloadAsync(urls[i], onsuccess, function(x) {
+      console.log('FAIL: ', x);
+      tryNextURL(i + 1);
+    });
+  }
+
+  tryNextURL(0);
+}
+
+
+
+downloadOneAsync(urls,
+  function(x) {
+    console.log("SUCCESS ", x);
+  },
+  function(x) {
+    console.log("FAILURE ", x);
+  });
