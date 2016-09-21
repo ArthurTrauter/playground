961eb6212b0594466ff811877149d262f5194cb4
ArthurTrauter <arthur.trauter@googlemail.com>
2016-09-21 09:19:42 +0200

    another react exercise

--- 39-e6katas/katas/26-class-more-extends.js
+++ 39-e6katas/katas/26-class-more-extends.js
@@ -18,28 +18,32 @@
     describe('prototypes are as you know them', () => {
       class A {}
       class B extends A {}
+
       it('A is the prototype of B', () => {
         const isIt = A.isPrototypeOf(B);
         assert.equal(isIt, true);
       });
+
       it('A`s prototype is also B`s prototype', () => {
-        const proto = Object.getPrototypeOf(B);
+        // const proto = Object.getPrototypeOf(B);
+        const proto = B.prototype;
         // Remember: don't touch the assert!!! :)
         assert.equal(A.prototype.isPrototypeOf(proto), true);
       });
     });
 
     describe('`extends` using an expression', () => {
+
       it('eg the inline assignment of the parent class', () => {
         let A;
-        class B extends(A = {}) {}
+        class B extends(A = class {}) {}
 
         assert.equal(new B() instanceof A, true);
       });
 
       it('or calling a function that returns the parent class', () => {
         const returnParent = (beNull) => beNull ? null : class {};
-        class B extends(returnParent) {}
+        class B extends(returnParent(true)) {}
 
         assert.equal(Object.getPrototypeOf(B.prototype), null);
       });
