a0a04e73cf02949a77d4edf6416f6639ca26690a
Arthur Trauter <arthur.trauter@sap.com>
2017-03-22 07:05:41 +0100

    46 karma testrunner

---
+++ 46-karma/test/firstTest.qunit.js
@@ -0,0 +1,16 @@
+(function ($) {
+	"use strict";
+
+	QUnit.module("CombinationLock API", {
+		beforeEach : function () {
+		},
+		afterEach : function () {
+		}
+	});
+
+	QUnit.test( "hello test", function( assert ) {
+		assert.ok( 1 == "1", "Passed!" );
+		assert.notOk(false, "Passed again");
+	});
+
+})();
