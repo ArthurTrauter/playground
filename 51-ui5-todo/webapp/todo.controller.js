cb5520028ca836c0b138b3bda630355f1d0c6bc7
Arthur Trauter <arthur.trauter@sap.com>
2017-11-27 22:09:29 +0100

    controller added

--- 51-ui5-todo/webapp/todo.controller.js
+++ 51-ui5-todo/webapp/todo.controller.js
@@ -1,11 +1,27 @@
-sap.ui.controller("app.todo", {
-	onInit: function() {
-		var oData = {
-			ToDos : [
-				{ title: "Start this app", completed: true }
-			]
-		};
-		var ccModel = new sap.ui.model.json.JSONModel(oData);
-		this.getView().setModel(ccModel, "cc");
-	}
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function(Controller, JSONModel) {
+	'use strict';
+
+	return Controller.extend('app.todo', {
+		onInit: function() {
+			var oData = {
+				ToDos : [
+					{ title: "Start this app", completed: true }
+				]
+			};
+			var ccModel = new sap.ui.model.json.JSONModel(oData);
+			this.getView().setModel(ccModel, "cc");
+		},
+		onSelectionChange: function() {
+			alert("onSelectionChange");
+		},
+		onPressClearCompleted: function() {
+			alert("onPressClearCompleted");
+		},
+		onSearch: function() {
+			alert("onSearch");
+		}
+	});
 });
